5e9cc37ddc0f92cff5bc427c90a73796cf5175fe
poyrazogluyigit <poyrazogluyigit@gmail.com>
2026-05-13 14:46:12 +0300

    minor changes

--- examples/basic-example/B.cpp
+++ examples/basic-example/B.cpp
@@ -1,6 +1,10 @@
 #include <thread>
 #include "zenoh.hxx"    // IWYU pragma: keep
 
+void C_callback(const zenoh::Sample&) {
+    return;
+}
+
 int main() {
 
     auto session = zenoh::Session::open(zenoh::Config::create_default());
@@ -8,7 +12,7 @@ int main() {
     auto A_pub = session.declare_publisher("basic/A/B");
     auto C_pub = session.declare_publisher("basic/C/B");
 
-    auto A_callback = [&](){
+    auto A_callback = [&](const zenoh::Sample&){
         for (int i = 0; i < 5; i++) {
             C_pub.put("example payload to C");
         }
@@ -16,11 +20,6 @@ int main() {
     };
 
     auto A_sub = session.declare_subscriber("basic/B/A", &A_callback, zenoh::closures::none);
-
-    auto C_callback = [&](){
-        // do nothing
-    };
-
     auto C_sub = session.declare_subscriber("basic/B/C", &C_callback, zenoh::closures::none);
 
 
